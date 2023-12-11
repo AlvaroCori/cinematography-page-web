1c62b607fde23cd38ee00e2d9626a8094a726209
AlvaroCori <alvaro.cori@niceincontact.com>
2023-12-11 18:07:54 -0400

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const fetch = require("node-fetch");
 function App() {
+  const url = 'https://api.themoviedb.org/3/authentication';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzY2MGU4ZDNmNWM0OTdjMDgzZGRmYjc4ZWRlODdhZCIsInN1YiI6IjY1Nzc3ZGU5NTY0ZWM3MDEzOGJjYmMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0YkQj9DxXmPDjgQtyT7HBY6IfP5WiupweuuouuijQzY'
+    }
+  };
+
+  fetch(url, options)
+    .then((res : any) => res.json())
+    .then((json : any) => console.log(json))
+    .catch((err : any) => console.error('error:' + err));
+
+  let urlMovies = 'https://api.themoviedb.org/3/movie/5?language=en-US';
+  let optionsMovies = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzY2MGU4ZDNmNWM0OTdjMDgzZGRmYjc4ZWRlODdhZCIsInN1YiI6IjY1Nzc3ZGU5NTY0ZWM3MDEzOGJjYmMxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0YkQj9DxXmPDjgQtyT7HBY6IfP5WiupweuuouuijQzY'
+    }
+  };
+    
+  fetch(urlMovies, optionsMovies)
+  .then((res : any) => res.json())
+  .then((json : any) => console.log(json))
+  .catch((err : any) => console.error('error:' + err));
+
   return (
     <div className="App">
       <header className="App-header">
